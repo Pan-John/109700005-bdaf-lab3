3d9dffebeb4f4d1fa2f2909e058c1bf8fe82ec6e
Pan <125814787+Pan-John@users.noreply.github.com>
2023-03-21 04:02:48 +0800

    Update bank.sol

--- contract/bank.sol
+++ contract/bank.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+contract Bank {
+    mapping(address account_address => mapping(address token_address => uint256)) public _balances;
+
+    function deposit(address token, uint256 amount) external {
+        _balances[msg.sender][token] += amount;
+        ERC20(token).transferFrom(msg.sender, address(this), amount);
+    }
+
+    function withdraw(address token, uint256 amount) external {
+        require(_balances[msg.sender][token] >= amount, "Insufficent balance!");
+        _balances[msg.sender][token] -= amount;
+        ERC20(token).transfer(msg.sender, amount);
+    }
+}
