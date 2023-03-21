6d7c7b432cc9853d1b752c56b87f107b313ef561
Pan <125814787+Pan-John@users.noreply.github.com>
2023-03-21 23:38:54 +0800

    Update token.sol

--- contracts/token.sol
+++ contracts/token.sol
@@ -2,14 +2,13 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Ptc is ERC20, Ownable {
+contract Ptc is ERC20 {
     constructor() ERC20('PTC', 'PeTeCoin') {
          _mint(msg.sender, 5000);
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
 }
