7438423e8e009e824a0bde554a7c00fc73853da2
Pan <125814787+Pan-John@users.noreply.github.com>
2023-03-21 13:39:10 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,19 @@
-require('dotenv').config();
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
+
+const GOERLI_API_KEY=process.env.GOERLI_API_KEY;
+const MTMS_PRIVATE_KEY=process.env.MTMS_PRIVATE_KEY;
+const ETHERSCAN_API_KEY=process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   solidity: "0.8.18",
   networks: {
     sepolia: {
-      url: `https://sepolia.infura.io/v3/${process.env.GOERLI_API_KEY}`,
-      accounts: [ process.env.MTMS_PRIVATE_KEY],
+      url: `https://sepolia.infura.io/v3/${GOERLI_API_KEY}`,
+      accounts: [MTMS_PRIVATE_KEY],
     },
   },
   etherscan:{
-    apiKey:  process.env.ETHERSCAN_API_KEY
+    apiKey: ETHERSCAN_API_KEY
   }
 };
